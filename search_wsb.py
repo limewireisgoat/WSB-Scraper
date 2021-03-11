2df8ca80b6a04f6c861f6610567161a8804cb9ff
Kenji Niyokindi <kniyokindi@gmail.com>
2021-03-12 01:01:15 +0300

    Scraper code modified and updated

--- search_wsb.py
+++ search_wsb.py
@@ -11,28 +11,30 @@ start_time=int(datetime.datetime(2020, 1, 1).timestamp())
 submissions = (api.search_submissions(after=start_time,
                             subreddit='wallstreetbets',
                             filter = ['title', 'selftext', 'num_comments', 'score', 'description'],
-                            limit = 50))
+                            limit = 5000))
 
-
-dict = {"Title": None,"#Comments": None, "Score": None, "Symbols": None}
 wsb_dict = {"Data":[]}
 
 
 for submission in submissions:
-    words = submission.selftext.split()
-    cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
-
-    if len(cashtags) > 0:
+    if hasattr(submission, 'selftext'):
+        dict = {"Title": None, "#Comments": None, "Score": None, "Symbols": None}
+        words = submission.selftext.split()
+        for word in submission.title.split():
+            words.append(word)
+        cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
+        cashtags = list(set(filter(lambda word: word[1:].isalpha(), cashtags)))
         
-        dict["Title"] = submission.title
-        dict["#Comments"] = submission.num_comments
-        dict["Score"] = submission.score
-        dict["Symbols"]=cashtags
-        wsb_dict["Data"].append(dict)
+        if len(cashtags) > 0:
+            dict["Title"] = submission.title
+            dict["#Comments"] = submission.num_comments
+            dict["Score"] = submission.score
+            dict["Symbols"]= cashtags
+            wsb_dict["Data"].append(dict)
 
-print(wsb_dict)
+#print(wsb_dict)
 
 #JSON DATA DUMPING
 with open('wsb.json', 'w') as fp:
-    json.dump(wsb_dict, fp, indent=1)
-    
+   json.dump(wsb_dict, fp, indent=1)
+ 
