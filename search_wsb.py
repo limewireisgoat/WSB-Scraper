479d5ef3cc06f7f7db19f8da2965d298ce5cc192
Kenji Niyokindi <kniyokindi@gmail.com>
2021-03-11 20:15:24 +0300

    Search_wsb.py file updated

--- search_wsb.py
+++ search_wsb.py
@@ -1,3 +1,4 @@
+import json
 import psaw
 
 from psaw import PushshiftAPI
@@ -9,19 +10,29 @@ start_time=int(datetime.datetime(2020, 1, 1).timestamp())
 
 submissions = (api.search_submissions(after=start_time,
                             subreddit='wallstreetbets',
-                            filter = ['title', 'selftext', 'num_comments']
-                            ))
+                            filter = ['title', 'selftext', 'num_comments', 'score'],
+                            limit = 50))
+
+
+dict = {"Title": None, "#Comments": None, "Score": None, "Symbols": None}
+wsb_dict = {"Data":[]}
+
+
 for submission in submissions:
     words = submission.selftext.split()
     cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
 
     if len(cashtags) > 0:
-        print(submission.description)
-        print("Title :" + submission.title)
-        print("# Comment: {}".format(submission.num_comments))
-        print(cashtags)
-    
-   
+        
+        dict["Title"] = submission.title
+        dict["#Comments"] = submission.num_comments
+        dict["Score"] = submission.score
+        dict["Symbols"]=cashtags
+        wsb_dict["Data"].append(dict)
 
 
+
+#JSON DATA DUMPING
+with open('wsb.json', 'w') as fp:
+    json.dump(wsb_dict, fp, indent=1)
     
