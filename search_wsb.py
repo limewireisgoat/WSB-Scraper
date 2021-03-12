706436f84e2882ca16c40e2bbbed18e62fdb8a45
Kenji Niyokindi <kniyokindi@gmail.com>
2021-03-13 01:26:22 +0300

    Scraper code updated

--- search_wsb.py
+++ search_wsb.py
@@ -11,26 +11,29 @@ start_time=int(datetime.datetime(2020, 1, 1).timestamp())
 submissions = (api.search_submissions(after=start_time,
                             subreddit='wallstreetbets',
                             filter = ['title', 'selftext', 'num_comments', 'score', 'description'],
-                            limit = 5000))
+                            limit = 100))
 
 wsb_dict = {"Data":[]}
 
 
 for submission in submissions:
     if hasattr(submission, 'selftext'):
-        dict = {"Title": None, "#Comments": None, "Score": None, "Symbols": None}
-        words = submission.selftext.split()
-        for word in submission.title.split():
-            words.append(word)
-        cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
-        cashtags = list(set(filter(lambda word: word[1:].isalpha(), cashtags)))
+            if "[deleted]" not in submission.selftext and "[removed]" not in submission.selftext:
+                dict = {"Title": None, "#Comments": None, "Score": None, "Symbols": None}
+                words = submission.selftext.split()
+                for word in submission.title.split():
+                    words.append(word)
+                cashtags = list(set(filter(lambda word: word.lower().startswith("$"), words)))
+                cashtags = list(set(filter(lambda word: word[1:].isalpha(), cashtags)))
         
-        if len(cashtags) > 0:
-            dict["Title"] = submission.title
-            dict["#Comments"] = submission.num_comments
-            dict["Score"] = submission.score
-            dict["Symbols"]= cashtags
-            wsb_dict["Data"].append(dict)
+                if len(cashtags) > 0:
+                    dict["Title"] = submission.title
+                    dict["#Comments"] = submission.num_comments
+                    dict["Score"] = submission.score
+                    dict["Symbols"]= cashtags
+                    wsb_dict["Data"].append(dict)
+
+        
 
 #print(wsb_dict)
 
